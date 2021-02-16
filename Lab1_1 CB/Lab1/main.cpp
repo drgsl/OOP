c49433d8e5acd4db07259b1992f7610b0a4f5e7c
BobuDragos <46262576+BobuDragos@users.noreply.github.com>
2021-02-16 19:07:41 +0200

    lab11.2

--- Lab1_1 CB/Lab1/main.cpp
+++ Lab1_1 CB/Lab1/main.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+int convert_str_to_int(char str[]){
+cout<<str<<" ";
+return 0;
+}
 
 
 int main()
@@ -11,5 +15,13 @@ int main()
     if(!f)
         perror("Error on reading! Error message: "), exit(1);
 
+    int sum;
+
+    for(int i= 0; i<4; i++)
+    {
+        char buf[10];
+        fscanf(f,"%s", &buf);
+        sum+=convert_str_to_int(buf);
+    }
     return 0;
 }
