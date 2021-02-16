5ff5cd107a99d25e8fba2f413f00ebdc120b71db
BobuDragos <46262576+BobuDragos@users.noreply.github.com>
2021-02-16 19:39:02 +0200

    lab12.2

--- Lab1_1 CB/Lab1.2/main.cpp
+++ Lab1_1 CB/Lab1.2/main.cpp
@@ -3,23 +3,44 @@
 
 using namespace std;
 
-struct word{
+struct word
+{
 
-char txt[32];
+    char txt[32];
 
 };
 
 int main()
 {
-    char str[255];
-    scanf("%[^\n]s", str);
+    char str[255] = "I went to the library yesterday";
+    //scanf("%[^\n]s", str);
 
     char *p = strtok(str, " ");
 
     word words[10];
-    while(p){
-            cout<< p<<endl;
+    int i = 0;
+
+    while(p)
+    {
+        strcpy(words[i++].txt, p);
         p = strtok(NULL, " ");
     }
+
+    for(int i = 0; i<6; i++)
+    {
+        for(int j = 5; j >=0; j--)
+        {
+            if(strlen(words[j].txt) > strlen(words[i].txt))
+            {
+                ///Swap
+                char aux[32] = "";
+                strcpy(aux, words[i].txt);
+                strcpy(words[i].txt, words[j].txt);
+                strcpy(words[j].txt, aux);
+            }
+        }
+    }
+    for(int i = 0; i<6; i++)
+        cout<<words[i].txt<<endl;
     return 0;
 }
