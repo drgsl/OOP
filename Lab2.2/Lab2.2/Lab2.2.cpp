678d569cb059871223a2314ec8904db4b3b6530b
BobuDragos <46262576+BobuDragos@users.noreply.github.com>
2021-02-23 19:31:49 +0200

    lab2.2.3

--- Lab2.2/Lab2.2/Lab2.2.cpp
+++ Lab2.2/Lab2.2/Lab2.2.cpp
@@ -5,6 +5,48 @@
 #include "Student.h"
 #include <cstring>
 
+
+int compareNames(Student s1, Student s2) {
+    if (strlen(s1.getName()) > strlen(s2.getName()))
+        return 1;
+    else if (strlen(s1.getName()) < strlen(s2.getName()))
+        return -1;
+    else
+        return 0;
+}
+int compareGradeMath(Student s1, Student s2) {
+    if (s1.getGradeMath() > s2.getGradeMath())
+        return 1;
+    else if (s1.getGradeMath() < s2.getGradeMath())
+        return -1;
+    else
+        return 0;
+}
+int compareGradeEnglish(Student s1, Student s2) {
+    if (s1.getGradeEnglish() > s2.getGradeEnglish())
+        return 1;
+    else if (s1.getGradeEnglish() < s2.getGradeEnglish())
+        return -1;
+    else
+        return 0;
+}
+int compareGradeHistory(Student s1, Student s2) {
+    if (s1.getGradeHistory() > s2.getGradeHistory())
+        return 1;
+    else if (s1.getGradeHistory() < s2.getGradeHistory())
+        return -1;
+    else
+        return 0;
+}
+int compareGradeAverage(Student s1, Student s2) {
+    if (s1.getGradeAverage() > s2.getGradeAverage())
+        return 1;
+    else if (s1.getGradeAverage() < s2.getGradeAverage())
+        return -1;
+    else
+        return 0;
+}
+
 int main()
 {
     Student s1;
@@ -21,34 +63,30 @@ int main()
     std::cout << s1.getGradeHistory() << std::endl;
 
     std::cout << s1.getGradeAverage() << std::endl;
-}
 
-int compareNames(Student s1, Student s2) {
-    if (strlen(s1.getName()) > strlen(s2.getName()))
-        return 1;
-    else if (strlen(s1.getName()) < strlen(s2.getName()))
-        return -1;
-    else
-        return 0;
-}
+    Student s2;
+    s2.setName("Bogdan");
+    std::cout << s2.getName() << std::endl;
 
-int compareGradeMath(Student s1, Student s2) {
-    if (s1.getGradeMath() > s2.getGradeMath())
-        return 1;
-    else if (s1.getGradeMath() < s2.getGradeMath())
-        return -1;
-    else
-        return 0;
-}
+    s2.setGradeMath(7);
+    std::cout << s2.getGradeMath() << std::endl;
 
-int compareGradeMath(Student s1, Student s2) {
-    if (s1.getGradeMath() > s2.getGradeMath())
-        return 1;
-    else if (s1.getGradeMath() < s2.getGradeMath())
-        return -1;
-    else
-        return 0;
-}
+    s2.setGradeEnglish(6);
+    std::cout << s2.getGradeEnglish() << std::endl;
+
+    s2.setGradeHistory(5);
+    std::cout << s2.getGradeHistory() << std::endl;
+
+    std::cout << s2.getGradeAverage() << std::endl;
+
+    std::cout << std::endl;
+    std::cout << compareNames(s1, s2) << std::endl;
+    std::cout << compareGradeMath(s1, s2) << std::endl;
+    std::cout << compareGradeEnglish(s1, s2) << std::endl;
+    std::cout << compareGradeHistory(s1, s2) << std::endl;
+    std::cout << compareGradeAverage(s1, s2) << std::endl;
+
+} 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
