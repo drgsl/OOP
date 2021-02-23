7a8a513a2c19dbb3b78447f05848d10a12cbd401
BobuDragos <46262576+BobuDragos@users.noreply.github.com>
2021-02-23 18:38:04 +0200

    lab2.1.2

--- Lab2.1/Lab2.1/NumberList.h
+++ Lab2.1/Lab2.1/NumberList.h
@@ -1,17 +1,45 @@
 #pragma once
+#include <stdio.h>
 class NumberList
 {
     int numbers[10];
     int count;
 public:
-    void Init();          // count will be 0
-    bool Add(int x);      // adds X to the numbers list and increase the data member count.
-                          // if count is bigger or equal to 10, the function will return false
-    void Sort();          // will sort the numbers vector
-    void Print();         // will print the current vector
 
+    // count will be 0
     void Init() {
         count = 0;
     }
+
+    // adds X to the numbers list and increase the data member count.
+    // if count is bigger or equal to 10, the function will return false
+    bool Add(int x) {
+        if (count >= 10)
+            return false;
+
+        numbers[count++] = x;
+    }
+
+    // will sort the numbers vector
+    bool Sort() {
+        for (int i = 0; i < count; i++) {
+            for (int j = i; j > 0; j++) {
+                if (numbers[i] > numbers[j])
+                {
+                    int aux = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = aux;
+                }
+            }
+        }
+    }
+
+    // will print the current vector
+    void Print() {
+        for (int i = 0; i < count; i++) {
+            printf("%d", numbers[i]);
+        }
+    }
+
 };
 
